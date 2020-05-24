984343a9382dfd0dfb2146b71f6deb4128e9bd2d
Huldar <44577486+G0rocks@users.noreply.github.com>
2020-05-24 21:16:41 +0200

    Add files via upload
    First commit. Tetris theme song downloaded from some online site distributing it.
    Tetris code version 1 includes comments to organise the code.

---
+++ Tetris/Tetris.py
@@ -0,0 +1,40 @@
+"""
+**************************************************************************************************************
+Höfundur:       Huldar
+Dagsetning:     24.maí 2020
+Forrit:         Tetris í python
+Upplýsingar:    Upphaflegi tetris leikurinn: https://is.wikipedia.org/wiki/Tetris
+                Reglurnar um hvað formin heita, liti, stigagjöf og fleira er hægt að finna á 
+                ensku síðunni á wikipedia um tetris.
+**************************************************************************************************************
+"""
+
+# Útbúum tetrimino-a
+    # Tetrimino
+
+# Búum til skjá og setjum leikskjá inn í hann
+
+# Gerum upphafsskjá sem leyfir þér að smella á "Spila"
+
+# Sínum til hliðar, í litlum glugga, hvaða tetrimino er næstur. Tetrimino-ar eru valdir af handahófi
+
+# Setjum tetrimino inn í skjáinn einn í einu
+
+# Látum tetrimino-ana detta, einn í einu, rólega niður
+
+# Snúum tetrimino-inum þegar leikmaðurinn ýtir á upp örina
+
+# Færum tetrimino-inn til hægri/vinstri þegar leikmaðurinn ýtir á hægri/vinstri örvatakkana
+
+# Látum tetrimino-inn detta hratt niður á meðan leikmaðurinn heldur niðri niður örvatakkanum
+
+# Setja þá alla leið niður ef leikmaður ýtir á bilslánna
+
+# Láta tetrimino-a stoppa þegar þeir rekast á annan tetrimino fyrir neðan sig
+    # Setja næsta tetrimino af stað þegar núverandi tetrimino stoppar
+
+# Þegar tetriminoarnir mynda heila, óbrotna, lárétta línu yfir allan leikskjáinn þá hverfa þeira og leikmaðurinn fær
+# stig í samræmi við hversu margar línur hurfu samtímis
+    # Láta alla kubba fyrir ofan línuna detta niður þegar línan/línurnar hverfa
+
+# Spila tetris lagið í bakgrunninum á meðan leikurinn er í gangi
