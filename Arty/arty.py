90252ad561f5be5bafa75dd0cbb032a43cefe9cf
Huldar <huldarh@gmail.com>
2021-05-23 22:12:00 +0000

    Displays image with colors

--- Arty/arty.py
+++ Arty/arty.py
@@ -1,9 +1,27 @@
-from PIL import Image
+import numpy
+import cv2
+import time
 
 width = 250
 height = width
 mynd_size = (width, height)
 
-mynd = Image.new(mode = "RGB", size = mynd_size)
+R = 9
+G = 150
+B = 109
+litur = (R,G,B)
 
-mynd.show()
+X = range(width)
+Y = range(height)
+
+mynd = numpy.full((height,width,3), litur, dtype=numpy.uint8)
+for x in X:
+  for y in Y:
+    R = x
+    G = y
+    B = 109
+    litur = (R,G,B)
+    mynd[x,y] = litur
+
+cv2.imshow('image',mynd)
+cv2.waitKey(0)
